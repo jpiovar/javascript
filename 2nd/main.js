c54f66465e5ec70a86c8c95f50a20749c04f1f85
Juraj Piovar <jpiovar@sk.ibm.com>
2019-07-22 14:45:52 +0200

    javascript export import module to module to index

---
+++ 2nd/main.js
@@ -0,0 +1,7 @@
+import {wholeName} from './utils.js';
+
+export function wholeNameExtended(wN) {
+    return `${wN} je sekac :)`;
+}
+
+export const preparedWholeName = wholeNameExtended(wholeName);
